d1b03743e8fd48bdcb1c897a0e30a13667f54861
Ventru <ventru67@gmail.com>
2023-08-11 14:58:09 +0300

    Print Operation Done

---
+++ GB Python lesson7 homework/ex02.py
@@ -0,0 +1,9 @@
+#Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца.
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    print(a)
+    for i in a:
+        print(*[f"{x:>3}" for x in i])
+
+print_operation_table(lambda x, y: x * y)
